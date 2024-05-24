cad7adbd51c639b0680e8a0828ff37700a3b076a
Gavin <gavinwacira@gmail.com>
2024-05-24 13:53:13 +0300

    strin

--- main.java
+++ main.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 public class main {
     public static void main(String[] args) {
-        Hashtable<Integer, String> table = new Hashtable<>(10);
+        Hashtable<String, String> table = new Hashtable<>(10);
 
-        table.put(100, "Brian");
-        table.put(123, "Mary");
-        table.put(321, "Sandy");
-        table.put(555, "Ian");
-        table.put(777, "Kevin");
+        table.put("100", "Brian");
+        table.put("123", "Mary");
+        table.put("321", "Sandy");
+        table.put("555", "Ian");
+        table.put("777", "Kevin");
         
         //table.remove(777);
 
-        for (Integer key : table.keySet()) {
+        for (String key : table.keySet()) {
             System.out.println(key.hashCode() % 10 + "\t" + key  + "\t" + table.get(key)); // Use the loop variable 'key' instead of 'Key'
         }
     }
